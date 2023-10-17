1ee538875d5483cff8e84280554d9a2589c62df5
Guled24 <gmuse12@hotmail.com>
2023-10-17 21:00:31 +0100

    .lesson 1 of javascript.

---
+++ Lessons/javascript/lesson1.js
@@ -0,0 +1,34 @@
+//this is a comment
+
+/*this
+will 
+be a 
+double line
+comment */
+
+console.log("Hello World") //text//
+
+console.log(12) //number//
+
+console.log(false) //bolean//
+
+var name = "Guled" //variable// 
+console .log(name) //prints the variable//
+
+const name = "Asia" // it keeps the name constant and wont change//
+
+let part2name = "Hi" // can be renamed and moved 
+/*if the 'let' was added and changed below the top 'let' 
+will stay the same but the bottom 'let' will change */
+
+// data-types
+// " "/' '/ " -  string
+
+// 0-9 number data-type
+
+// true/false  bolean data type
+
+// undefined data type eg var today;
+
+// console.log(type of) - return what the code is made up off
+
